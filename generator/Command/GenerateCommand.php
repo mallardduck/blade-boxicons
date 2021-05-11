81175ead0a5c31019ec92d5e8c35bfb1100b9b43
Dan Pock <self@danpock.me>
2021-05-11 18:13:50 -0400

    refactor to use common generator base

--- generator/Command/GenerateCommand.php
+++ generator/Command/GenerateCommand.php
@@ -4,118 +4,21 @@ namespace MallardDuck\BladeBoxicons\Generator\Command;
 
 use DOMDocument;
 use DOMElement;
-use Illuminate\Support\Str;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputDefinition;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use BladeUI\Icons\Console\AbstractGenerateCommand;
 
-class GenerateCommand extends Command
+class GenerateCommand extends AbstractGenerateCommand
 {
-    private static string $baseDir;
-
-    const SVG_SOURCE_BASE = '/node_modules/boxicons/svg';
-    const TMP_DEST_BASE = '/build';
-    const SVG_DEST_BASE = '/resources/svg';
-
-    const TYPES = [
-        '/logos',
-        '/regular',
-        '/solid',
+    protected static string $npmPackage = 'boxicons';
+    protected static array $iconSets = [
+        'regular' => 'bx-',
+        'logos' => 'bxl-',
+        'solid' => 'bxs-',
     ];
 
-    const TYPE_MAP = [
-        'logos' => 'l',
-        'regular' => null,
-        'solid' => 's',
-    ];
-
-    protected static $defaultName = "generate";
-
-    public function __construct(string $name = null)
-    {
-        self::$baseDir = dirname(__DIR__, 2);
-        parent::__construct($name);
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        if (!is_dir(self::$baseDir . self::SVG_SOURCE_BASE)) {
-            $output->writeln("The node_modules folder doesn't exist");
-            return Command::FAILURE;
-        }
-        $this->ensureDirExists(self::$baseDir . self::TMP_DEST_BASE);
-
-        $output->writeln("Starting to discover source SVGs...");
-
-        foreach (self::TYPES as $type) {
-            $output->writeln("Processing SVGs for {$type} type svgs.");
-            // Setup build dir for type
-            $this->ensureDirExists(self::$baseDir . self::TMP_DEST_BASE . $type);
-
-            $fileTransformationList = $this->getDirectoryFileList(self::$baseDir . self::SVG_SOURCE_BASE . $type);
-            $this->updateSvgs($type, $fileTransformationList);
-            $output->writeln("Completed processing for {$type} svgs.");
-        }
-
-        $output->writeln("Cleaning up the build directory...");
-        $this->deleteDirectory(self::$baseDir . self::TMP_DEST_BASE);
-        $output->writeln("Done!");
-
-        return Command::SUCCESS;
-    }
-
-    private function ensureDirExists(string $dirPath)
-    {
-        if (!is_dir($dirPath)) {
-            if (!mkdir($dirPath) && !is_dir($dirPath)) {
-                throw new \RuntimeException(sprintf('Directory "%s" was not created', $dirPath));
-            }
-        }
-    }
-
-    private function getDirectoryFileList(string $dirPath): array
-    {
-        $prefix = sprintf("bx%s-", self::TYPE_MAP[Str::afterLast($dirPath, DIRECTORY_SEPARATOR)]);
-        // Scan for files...
-        $filesList = scandir($dirPath);
-        // Shift out the "." and ".." items.
-        array_shift($filesList);
-        array_shift($filesList);
-
-        // modify things to a tuple:
-        $filesList = collect($filesList)
-            ->map(static fn($value) => [$value, Str::after($value, $prefix)]);
-
-        return $filesList->toArray();
-    }
-
-    private function updateSvgs(string $type, array $fileTransformations): void
-    {
-        $typeTempDir = self::$baseDir . self::TMP_DEST_BASE . $type;
-
-        foreach ($fileTransformations as $fileTransformation) {
-            [$orgFile, $newFile] = $fileTransformation;
-            // Set path variables...
-            $sourceFile = self::$baseDir . self::SVG_SOURCE_BASE . $type . '/' . $orgFile;
-            $tempFile = $typeTempDir . '/' . $newFile;
-            $finalFile = self::$baseDir . self::SVG_DEST_BASE . $type . '/' . $newFile;
-
-            // Copy file to temp...
-            copy($sourceFile, $tempFile); // Stage 1
-            $this->normalizeSvgFile($tempFile); // Stage 2
-            // Start stage 3
-            $svgLine = trim(file($tempFile)[1]);
-            file_put_contents($tempFile, $svgLine);
-            // Final stage...
-            copy($tempFile, $finalFile);
-        }
-    }
-
-    private function normalizeSvgFile(string $tempFile)
+    public function normalizeSvgFile(string $tempFilepath, string $iconSet): void
     {
         $doc = new DOMDocument();
-        $doc->load($tempFile);
+        $doc->load($tempFilepath);
         /**
          * @var DOMElement $svgElement
          */
@@ -126,14 +29,9 @@ class GenerateCommand extends Command
         $svgElement->setAttribute('fill', 'currentColor');
         $svgElement->setAttribute('stroke', 'none');
         $svgElement->setAttribute('viewBox', '0 0 24 24');
-        $doc->save($tempFile);
-    }
+        $doc->save($tempFilepath);
 
-    private function deleteDirectory(string $directory) {
-        $files = array_diff(scandir($directory), array('.','..'));
-        foreach ($files as $file) {
-            (is_dir("$directory/$file")) ? $this->deleteDirectory("$directory/$file") : unlink("$directory/$file");
-        }
-        return rmdir($directory);
+        $svgLine = trim(file($tempFilepath)[1]);
+        file_put_contents($tempFilepath, $svgLine);
     }
 }
