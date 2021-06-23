60e47055631340533f07796e080fa4d3103c190c
Dan Pock <self@danpock.me>
2021-06-23 17:25:47 -0400

    remove traces of initial generator config

--- config/generation.php
+++ config/generation.php
@@ -1,34 +1,41 @@
 <?php
 
-use BladeUI\Icons\Generation\IconGenerator;
-use BladeUI\Icons\Generation\IconSetConfig;
+$svgNormalization = static function (string $tempFilepath, array $iconSet) {
+    $doc = new DOMDocument();
+    $doc->load($tempFilepath);
+    $svgElement = $doc->getElementsByTagName('svg')[0];
+    $svgElement->removeAttribute('width');
+    $svgElement->removeAttribute('height');
+    $svgElement->removeAttribute('viewBox');
+    $svgElement->setAttribute('fill', 'currentColor');
+    $svgElement->setAttribute('stroke', 'none');
+    $svgElement->setAttribute('viewBox', '0 0 24 24');
+    $doc->save($tempFilepath);
 
-return IconGenerator::create('blade-boxicons')
-    ->fromNPM('boxicons') // Optionally specify an `npm` package to load from
-    ->directory('/svg') // Specify a directory, if an npm package isn't set, this can be anywhere
-    ->withIconSets([
-        IconSetConfig::create('regular')
-            ->setInputFilePrefix('bx-'),
-        IconSetConfig::create('logos')
-            ->setInputFilePrefix('bxl-'),
-        IconSetConfig::create('solid')
-            ->setInputFilePrefix('bxs-'),
-    ])
-    ->withSvgNormalisation(function (string $tempFilepath, IconSetConfig $iconSet) {
-        $doc = new DOMDocument();
-        $doc->load($tempFilepath);
-        /**
-         * @var DOMElement $svgElement
-         */
-        $svgElement = $doc->getElementsByTagName('svg')[0];
-        $svgElement->removeAttribute('width');
-        $svgElement->removeAttribute('height');
-        $svgElement->removeAttribute('viewBox');
-        $svgElement->setAttribute('fill', 'currentColor');
-        $svgElement->setAttribute('stroke', 'none');
-        $svgElement->setAttribute('viewBox', '0 0 24 24');
-        $doc->save($tempFilepath);
+    $svgLine = trim(file($tempFilepath)[1]);
+    file_put_contents($tempFilepath, $svgLine);
+};
 
-        $svgLine = trim(file($tempFilepath)[1]);
-        file_put_contents($tempFilepath, $svgLine);
-    });
+return [
+    [
+        'source' => __DIR__.'/../node_modules/boxicons/svg/regular',
+        'destination' => __DIR__.'/../resources/svg/regular',
+        'input-prefix' => 'bx-',
+        'after' => $svgNormalization,
+        'safe' => true,
+    ],
+    [
+        'source' => __DIR__.'/../node_modules/boxicons/svg/logos',
+        'destination' => __DIR__.'/../resources/svg/logos',
+        'input-prefix' => 'bxl-',
+        'after' => $svgNormalization,
+        'safe' => true,
+    ],
+    [
+        'source' => __DIR__.'/../node_modules/boxicons/svg/solid',
+        'destination' => __DIR__.'/../resources/svg/solid',
+        'input-prefix' => 'bxs-',
+        'after' => $svgNormalization,
+        'safe' => true,
+    ],
+];
