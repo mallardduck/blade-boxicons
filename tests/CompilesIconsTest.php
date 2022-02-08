c0f7e497994fd10b56293997316500cf746669b3
Dan Pock <dpock@liquidweb.com>
2022-02-08 13:50:15 -0500

    try to fix OS issues with spatie snapshot package

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -7,6 +7,7 @@ namespace MallardDuck\BladeBoxicons\Tests;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use MallardDuck\BladeBoxicons\BladeBoxiconsServiceProvider;
 use Orchestra\Testbench\TestCase;
+use Spatie\Snapshots\Drivers\HtmlDriver;
 use Spatie\Snapshots\MatchesSnapshots;
 
 class CompilesIconsTest extends TestCase
@@ -17,21 +18,21 @@ class CompilesIconsTest extends TestCase
     public function it_compiles_a_single_anonymous_component()
     {
         $result = svg('bx-check-shield')->toHtml();
-        $this->assertMatchesHtmlSnapshot($result);
+        $this->assertMatchesTextSnapshot($result);
     }
 
     /** @test */
     public function it_can_add_classes_to_icons()
     {
         $result = svg('bx-check-shield', 'w-6 h-6 text-gray-500')->toHtml();
-        $this->assertMatchesHtmlSnapshot($result);
+        $this->assertMatchesTextSnapshot($result);
     }
 
     /** @test */
     public function it_can_add_styles_to_icons()
     {
         $result = svg('bx-check-shield', ['style' => 'color: #555'])->toHtml();
-        $this->assertMatchesHtmlSnapshot($result);
+        $this->assertMatchesTextSnapshot($result);
     }
 
     protected function getPackageProviders($app): array
